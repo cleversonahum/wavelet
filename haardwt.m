cc714b9542cc23568b01c843892a9e53ba75fc2e
Cleverson Nahum <cleversonahum@gmail.com>
2017-08-29 10:31:24 -0300

    função haardwt funcionando com números impares e pares e independente de ser multiplo de 2^n fixed #1

--- haardwt.m
+++ haardwt.m
@@ -1,16 +1,32 @@
 function [ output ] = haardwt( input )
 %HAARDWT Summary of this function goes here
 %   Detailed explanation goes here
+% L = length(input);
+% output = input;
+% while L>=1
+%     for i = 1:(L/2)
+%         output(i) = (input(2*i-1)+input(2*i))/sqrt(2);
+%         output((L/2)+i) = (input(2*i-1)-input(2*i))/sqrt(2);
+%     end
+%     input = output;
+%     L = L/2;
+% end
+%
 L = length(input);
-output = input;
-while L>=1
-    for i = 1:(L/2)
-        output(i) = (input(2*i-1)+input(2*i))/sqrt(2);
-        output((L/2)+i) = (input(2*i-1)-input(2*i))/sqrt(2);
+len = floor(L/2);
+output = zeros(1,L);
+while (true)
+    for i = 1:len
+       sum =  (input(2*i-1) + input(i * 2))/sqrt(2);
+       diff = (input(2*i-1) - input(i * 2))/sqrt(2);
+       output(i) = sum;
+       output(len+i) = diff;
     end
-    input = output;
-    L = L/2;
+    if len==1
+        return
+    end
+    input = output(1:len*2);
+    len = floor(len/2);
 end
-
 end
 
