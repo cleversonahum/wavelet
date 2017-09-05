2da0f180a9e6c864a993680a5cf3819dd1acb419
Derian Fernando <derian.sobral@gmail.com>
2017-09-05 18:11:57 +0000

    -Função aprimorada e comentada.

--- invhaardwt.m
+++ invhaardwt.m
@@ -1,7 +1,16 @@
 function [inverse] = invhaardwt(in,val,lev)
+
+% Entradas
+% in (vetor de entrada)
+% val (valor repassados pela função da transformada para facilitar o calculo da inversa)
+% lev (valor repassados pela função da transformada para facilitar o calculo da inversa)
+
+% Saidas
+% inverse (vetor de saida)
+
 len = val(lev);
 inverse = in;
-while(lev~=0)
+while(lev~=0) % Loop que fará a transformada inversa.
     in = inverse(1:len*2);    
     for i=1:len
         c = (in(i) + in(i+len))/sqrt(2);
