2eba13195b57b322748f35b052aef89d59c242de
Derian Fernando <derian.sobral@gmail.com>
2017-09-05 01:17:59 +0000

    -Hard threshold atualizada para suportar calculos por nivel de transformada.

--- hthreshold.m
+++ hthreshold.m
@@ -1,9 +1,16 @@
-function [vec] = hthreshold(vec)
+function [vec,stop] = hthreshold(vec,val,soma,stop)
 l=length(vec);
-t=sqrt(2*var(vec)*log(l)/l);
-for i=1:l
+sup = ones(1,val);
+k=1;
+for j=l-soma+1:stop
+    sup(k)=vec(j);
+    k=k+1;
+end
+t=sqrt(2*var(sup)*log(l)/l);
+for i= l-soma+1:stop
     if vec(i)<=t
         vec(i)=0.0;
     end
 end
+stop=l-soma;
 end
